1b7bd8f48375cf5de92e3714c354c0ce731ec45e
Gabriel Cordeiro <61151121+gabcordeiro@users.noreply.github.com>
2026-01-02 19:40:52 -0300

    ci: add flutter github actions workflow

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:beat_drop/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+///Função principal do BeatDrop.main
 void main() {
 runApp(
     const ProviderScope(
